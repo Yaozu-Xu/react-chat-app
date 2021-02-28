b6ad09d96b2888313ccc26c1481e1fab7cdb3f79
Yaozu-Xu <2589231996@qq.com>
2021-02-28 10:29:55 +0000

    Chore: Add reactSvgPlugin for showing svg

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import reactSvgPlugin from 'vite-plugin-react-svg'
 
 /**
  * https://vitejs.dev/config/
  * @type { import('vite').UserConfig }
  */
 export default {
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh(), reactSvgPlugin()],
 }

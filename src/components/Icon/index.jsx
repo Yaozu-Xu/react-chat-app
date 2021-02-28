fb26360341c4a02eab8ef3b4576dc32f8f6a8c11
Yaozu-Xu <2589231996@qq.com>
2021-02-28 10:31:42 +0000

    Feature: Create UI component of Icon

---
+++ src/components/Icon/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import IconWrapper from './style'
+
+function Icon({
+  icon: IconSvg,
+  width = 24,
+  height = 24,
+  opacity,
+  color,
+  ...rest
+}) {
+  return (
+    <IconWrapper color={color} opacity={opacity} {...rest}>
+
+      {IconSvg && <IconSvg width={width} height={height} />}
+
+    </IconWrapper>
+  )
+}
+
+Icon.propTypes = {
+  icon: PropTypes.func,
+  width: PropTypes.oneOfType([PropTypes.number, PropTypes
+    .string]),
+  height: PropTypes.oneOfType([PropTypes.number, PropTypes
+    .string]),
+  opacity: PropTypes.number,
+  color: PropTypes.string,
+}
+
+Icon.defaultProps = {
+  width: 24,
+  height: 24,
+}
+
+export default Icon

779a55ceb108a698e5127879bdd59dab8e51871d
Yaozu-Xu <2589231996@qq.com>
2021-02-28 10:31:03 +0000

    Chore: Add storybook settings for svg

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,5 +6,16 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: async (config, { configType }) => {
+    const assetRule = config.module.rules.find(({ test }) => test.test('.svg'));
+    const assetLoader = {
+      loader: assetRule.loader,
+      options: assetRule.options || assetRule.query,
+    };
+    config.module.rules.unshift({ test: /\.svg$/, use: ['@svgr/webpack', assetLoader] });
+
+    // Return the altered config
+    return config;
+  },
 }

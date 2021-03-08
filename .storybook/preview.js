3ba9a0952a306edc8106b9994d8f2216609409ad
Yaozu-Xu <2589231996@qq.com>
2021-03-08 11:02:09 +0000

    Feature: Set global style

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { addDecorator, addParameters } from '@storybook/react'
 import { ThemeProvider } from 'styled-components'
 import theme from '../src/theme'
+import '@/stories/global.css'
 
 addDecorator((storyFn) => (
   <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>

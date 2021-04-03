4dd799608ef116dceb86ed65d19cb825a99378b0
Yaozu-Xu <2589231996@qq.com>
2021-04-03 21:07:36 +0100

    Feature: Export components

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Avatar from './components/Avatar/index'
+import Badge from './components/Badge/index'
+import Icon from './components/Icon/index'
+import Navbar from './components/Navbar/index'
+
+export {
+  Avatar, Badge, Icon, Navbar,
+}

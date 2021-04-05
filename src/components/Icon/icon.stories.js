c050fff0879683e0b79d9584b1cbc64737d1e0c5
Yaozu-Xu <2589231996@qq.com>
2021-04-05 15:16:52 +0100

    Refactor: Remove fontawesome icons

--- src/components/Icon/icon.stories.js
+++ src/components/Icon/icon.stories.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { ReactComponent as SmileIcon } from '@/assets/icons/smile.svg'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCommentDots } from '@fortawesome/free-solid-svg-icons'
 import Icon from './index'
 
 export default {
@@ -14,41 +12,3 @@ export const Default = () => <Icon icon={SmileIcon} />
 export const CustomColor = () => <Icon icon={SmileIcon} color="#cccc" />
 
 export const CustomSize = () => <Icon icon={SmileIcon} width={48} height={48} />
-
-export const FontAwesome = () => <FontAwesomeIcon icon={faCommentDots} />
-
-export const FontAwesomeColor = () => (
-  <div>
-    <FontAwesomeIcon
-      icon={faCommentDots}
-      style={{ color: '#cccc', fontSize: '24px' }}
-    />
-    <FontAwesomeIcon
-      icon={faCommentDots}
-      style={{ color: 'orange', fontSize: '24px' }}
-    />
-    <FontAwesomeIcon
-      icon={faCommentDots}
-      style={{ color: 'green', fontSize: '24px' }}
-    />
-  </div>
-
-)
-
-export const FontAwesomeSize = () => (
-  <div>
-    <FontAwesomeIcon
-      icon={faCommentDots}
-      style={{ fontSize: '20px' }}
-    />
-    <FontAwesomeIcon
-      icon={faCommentDots}
-      style={{ fontSize: '24px' }}
-    />
-    <FontAwesomeIcon
-      icon={faCommentDots}
-      style={{ fontSize: '28px' }}
-    />
-  </div>
-
-)

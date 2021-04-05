bc1cb344c399b4d14d0f27b302eafb5e0476fc06
Yaozu-Xu <2589231996@qq.com>
2021-04-05 21:11:01 +0100

    Chore: Add resolve and commonjs plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,21 @@
 import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 
 export default {
   input: 'src/index.js',
   output: {
-    file: './lib/bundle.js', // should be the same as main in package.json
+    file: './lib/bundle.js',
     format: 'cjs',
   },
   plugins: [
-    // parse es6 react and emotion syntax
     babel({
       exclude: 'node_modules/**',
+    }),
+    resolve(),
+    commonjs({
+      ignoreGlobal: true,
+      include: /\/node_modules\//,
     }),
   ],
   external: ['react'], // exclude 3rd libraries to bundle with

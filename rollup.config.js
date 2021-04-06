7780a5866c3f979f0cb05131f68221a16da27d0f
Yaozu-Xu <2589231996@qq.com>
2021-04-06 09:34:24 +0100

    Chore: Spilit the build into bundles

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,33 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 
 export default {
-  input: 'src/index.js',
-  output: {
-    file: './lib/bundle.js',
-    format: 'cjs',
+  input: {
+    index: 'src/index',
+    Avatar: 'src/components/Avatar',
+    Badge: 'src/components/Badge',
+    Icon: 'src/components/Icon',
+    Navbar: 'src/components/Navbar',
+    Text: 'src/components/Text',
+    Button: 'src/components/Button',
+    Heading: 'src/components/Heading',
+    Option: 'src/components/Option',
+    Paragraph: 'src/components/Paragraph',
+    Filter: 'src/components/Filter',
+    Select: 'src/components/Select',
+    Card: 'src/components/Card',
   },
+  output: [{
+    dir: 'lib/esm',
+    format: 'esm',
+    exports: 'auto',
+  },
+  {
+    dir: 'lib/cjs',
+    format: 'cjs',
+    exports: 'auto',
+  },
+  ],
+
   plugins: [
     babel({
       exclude: 'node_modules/**',
